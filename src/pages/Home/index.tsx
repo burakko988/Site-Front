9f45215072b9482b88b4245ae73d092e3f45a832
MehmetAydar01 <mehmet.aydar07@gmail.com>
2023-10-24 08:02:26 +0300

    created home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Events from '../../components/Home/events/events';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>HOME</h1>
+      <Events />
+    </div>
+  )
+}
+
+export default Home;
