2390ec1dc9594b14f75b1902e197c0a096c9f660
MehmetAydar01 <mehmet.aydar07@gmail.com>
2023-10-24 08:02:17 +0300

    created event details page

---
+++ src/pages/EventDetails/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EventDetailsComp from '../../components/eventDetails/eventDetails';
+
+const EventDetails = () => {
+  return (
+    <div>
+      <EventDetailsComp />
+    </div>
+  )
+}
+
+export default EventDetails;
