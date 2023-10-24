0e97973502fe9d9cc353f4ebcb7f80bac367d8d5
MehmetAydar01 <mehmet.aydar07@gmail.com>
2023-10-24 08:01:34 +0300

    created last events page

---
+++ src/pages/LastEvents/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LastEventsComp from '../../components/lastEvents/lastEvents';
+
+const LastEvents = () => {
+  return (
+    <div>
+      <LastEventsComp />
+    </div>
+  )
+}
+
+export default LastEvents;
