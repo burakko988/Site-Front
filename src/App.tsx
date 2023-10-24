3a73d043968944e77d0e3d9673afe583f2f2ab60
MehmetAydar01 <mehmet.aydar07@gmail.com>
2023-10-24 07:59:28 +0300

    Router structure added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React from 'react';
+import ButtonAppBar from './components/navbar/navbar';
+import Home from './pages/Home';
+import EventDetails from './pages/EventDetails';
+import LastEvents from './pages/LastEvents';
 
-const App = () => {
-  
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+
+function App() {
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <div>
+        <ButtonAppBar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/event-details' element={<EventDetails />} />
+          <Route path='/last-events' element={<LastEvents />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
