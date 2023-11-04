4e5ecf08e515788d32edcdaaf9f58fefb5aa4025
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2023-11-04 15:20:43 +0300

    added CSS Baseline

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App.tsx';
+import { CssBaseline } from '@mui/material';
 import './index.css';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
+    <CssBaseline />
     <App />
   </QueryClientProvider>,
 );
