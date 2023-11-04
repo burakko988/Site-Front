21a91f8f22b613a3bb8c4d87a88afbe37384f27c
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2023-11-04 15:20:19 +0300

    Added Layout component

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,22 @@ import EventDetails from './pages/EventDetails';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ButtonAppBar } from './components/navbar/navbar';
-
+import Layout from './components/Layout/Layout';
+import EventList from './components/Event/eventList';
+import DynamicEventList from './components/Event/DynamicEventList';
 
 function App() {
-  const showOnHome = location.pathname === "/";
+  const showOnHome = location.pathname === '/';
   return (
     <Router>
-      <div>
-        <ButtonAppBar showOnHome={showOnHome} />
+      <Layout>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/event-details/:eventName' element={<EventDetails />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/event-details/:eventName" element={<EventDetails />} />
           {/* <Route path='/last-events' element={<LastEvents />} /> */}
+          <Route path="/category/:category" element={<DynamicEventList />} />
         </Routes>
-      </div>
+      </Layout>
     </Router>
   );
 }
