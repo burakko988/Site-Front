517d6af9009ea8ccdfe7aaef320f8397871fa29b
MehmetAydar01 <mehmet.aydar07@gmail.com>
2023-11-06 21:52:46 +0300

    created ticket detail for event

---
+++ src/components/DynamicEventDetails/ticketDetailContent.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+interface Props {
+  event: any;
+}
+
+const TicketDetailContent: React.FC<Props> = ({ event }) => {
+  
+  return (
+    <>
+      <section className="ticketDetail eventContent">
+        <div className="listHead">
+          <h2>Bilet Satın Al</h2>
+          <h2>Tarih Seçiniz</h2>
+        </div>
+        <div className="listDetail">
+          <section>
+            <h3>{event?.startDate.substring(0, 10)}</h3>
+            <p>{event?.location}</p>
+            <p>Kredi Kartlarına İndirim Avantajıyla</p>
+          </section>
+          <section>
+            <h3>{event?.ticketPrice}<span style={{ fontSize: '18px' }}>.00 ₺</span></h3>
+            <span>
+              <small>Başlayan Fiyatlar</small>
+            </span>
+          </section>
+          <section className="buyTicket">SATIN AL</section>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default TicketDetailContent;
