7301667af570f6592c6dbedb58a0ae77ce0bc589
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2023-11-06 15:18:37 +0300

    added previos event route

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import EventList from './components/Event/eventList';
 import DynamicEventList from './components/Event/DynamicEventList';
+import PreviousEvents from './pages/PreviousEvents/';
 
 function App() {
   const showOnHome = location.pathname === '/';
@@ -16,6 +17,7 @@ function App() {
           <Route path="/event-details/:eventName" element={<DynamicEventDetails />} />
           {/* <Route path='/last-events' element={<LastEvents />} /> */}
           <Route path="/category/:category" element={<DynamicEventList />} />
+          <Route path="/previous-events" element={<PreviousEvents />} />
         </Routes>
       </Layout>
     </Router>
