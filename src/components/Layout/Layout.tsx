bd477fc95e05d3ec79c2a8dd3c70151b8e86f24d
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2023-11-06 15:19:27 +0300

    fixed layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,6 +4,8 @@ import { AppBar, Toolbar, Typography, Container, Box } from '@mui/material';
 import ImageCarousel from '../Home/Slider/slider'; // Yolunuzun doğru olduğundan emin olun
 import EventCategories from '../Event/EventCategories';
 import { useLocation } from 'react-router-dom';
+import { Navbar } from '../Navbar/navbar';
+import { Link } from 'react-router-dom';
 
 interface LayoutProps {
   children: ReactNode;
@@ -14,12 +16,18 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   const isHomePage = location.pathname === '/';
   return (
     <>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6">My Application</Typography>
-        </Toolbar>
+      <AppBar position="sticky">
+        <Container>
+          <Toolbar>
+            <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+              <Typography variant="h6" component="h1">
+                My Application
+              </Typography>
+            </Link>
+          </Toolbar>
+        </Container>
       </AppBar>
-      <ImageCarousel />
+      {isHomePage && <ImageCarousel />}
       <EventCategories />
       <Container>
         <Box marginTop={2}>{children}</Box>
