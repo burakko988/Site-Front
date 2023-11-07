fe75798a7608038d09fa2a1f3e413e2d543997cc
busracogul <cogulbusra@gmail.com>
2023-11-07 14:01:37 +0300

    created-footer

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,6 +4,8 @@ import { AppBar, Toolbar, Typography, Container, Box } from '@mui/material';
 import ImageCarousel from '../Home/Slider/slider'; // Yolunuzun doğru olduğundan emin olun
 import EventCategories from '../Event/EventCategories';
 import { useLocation } from 'react-router-dom';
+import { Navbar } from '../navbar/navbar';
+import Footer from '../Footer/footer';
 
 interface LayoutProps {
   children: ReactNode;
@@ -14,16 +16,14 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   const isHomePage = location.pathname === '/';
   return (
     <>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6">My Application</Typography>
-        </Toolbar>
-      </AppBar>
+      <Navbar />
+
       <ImageCarousel />
       <EventCategories />
       <Container>
         <Box marginTop={2}>{children}</Box>
       </Container>
+      <Footer />
     </>
   );
 };
