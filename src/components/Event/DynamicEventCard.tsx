2db7b3ba56d23a2033825ae934c2809885941c96
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2023-11-16 00:19:51 +0300

    added card-component style

--- src/components/Event/DynamicEventCard.tsx
+++ src/components/Event/DynamicEventCard.tsx
@@ -32,57 +32,55 @@ function DynamicEventCard({ event }: DynamicEventCardProps) {
   const formattedEndDate = event.endDate && dayjs(event.endDate).isValid() ? dayjs(event.endDate).format('DD.MM.YYYY HH:mm') : '';
 
   return (
-    <div className="card-component">
+    <Card className="card-component">
       <Link to={`/event-details/${kebabTitle}`} state={{ id: _id }}>
-        <Card className="card-component">
-          <CardMedia component="img" height="180"  image={event.images[0]} alt="Event Image" />
-          <CardContent>
-            <Typography className="truncate-2" variant="h5" component="div">
-              {event.title}
-            </Typography>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <Box display="flex" alignItems="center">
-                  <LocationOnIcon color="primary" style={{ marginRight: 8 }} />
-                  <Typography className="truncate-1" variant="body2" color="textSecondary" component="span">
-                    {event.place}
-                  </Typography>
-                </Box>
-              </Grid>
-              <Grid item xs={12}>
-                <Box display="flex" alignItems="center">
-                  <EventIcon color="primary" style={{ marginRight: 8 }} />
-                  <Typography className="truncate-1" variant="body2" color="textSecondary" component="span">
-                    <h4>{formattedDate}</h4>
-                  </Typography>
-
-                  {formattedEndDate && <EventIcon color="primary" style={{ marginRight: 8 }} />}
-                  <Typography className="truncate-1" variant="body2" color="textSecondary" component="span">
-                    <h4>{formattedEndDate}</h4>
-                  </Typography>
-                </Box>
-              </Grid>
+        <CardMedia component="img" height="180" image={event.images[0]} alt="Event Image" />
+        <CardContent>
+          <Typography className="truncate-2" variant="h5" component="div">
+            {event.title}
+          </Typography>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Box display="flex" alignItems="center">
+                <LocationOnIcon color="primary" style={{ marginRight: 8 }} />
+                <Typography className="truncate-1" variant="body2" color="textSecondary" component="span">
+                  {event.place}
+                </Typography>
+              </Box>
             </Grid>
-            <Typography
-              variant="body2"
-              color="textSecondary"
-              component="p"
-              style={{
-                background: 'lightgray',
-                borderRadius: '8px',
-                padding: '8px',
-                display: 'inline-block',
-                width: '100%',
-                marginTop: '16px',
-                backgroundColor: '#2ED87B',
-              }}
-            >
-              Ticket Price: <span>{event.ticketPrice}</span>
-            </Typography>
-          </CardContent>
-        </Card>
+            <Grid item xs={12}>
+              <Box display="flex" alignItems="center">
+                <EventIcon color="primary" style={{ marginRight: 8 }} />
+                <Typography className="truncate-1" variant="body2" color="textSecondary" component="span">
+                  <h4>{formattedDate}</h4>
+                </Typography>
+
+                {formattedEndDate && <EventIcon color="primary" style={{ marginRight: 8 }} />}
+                <Typography className="truncate-1" variant="body2" color="textSecondary" component="span">
+                  <h4>{formattedEndDate}</h4>
+                </Typography>
+              </Box>
+            </Grid>
+          </Grid>
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            component="p"
+            style={{
+              background: 'lightgray',
+              borderRadius: '8px',
+              padding: '8px',
+              display: 'inline-block',
+              width: '100%',
+              marginTop: '16px',
+              backgroundColor: '#2ED87B',
+            }}
+          >
+            Ticket Price: <span>{event.ticketPrice}</span>
+          </Typography>
+        </CardContent>
       </Link>
-    </div>
+    </Card>
   );
 }
 
