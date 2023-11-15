c120a26c0714787e5c08d420cd1c9c67ff051eae
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2023-11-15 12:34:48 +0300

    created placepage

---
+++ src/pages/PlacePage/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import { fetchEventByPlace } from '../../services/eventService';
+import { useQuery } from 'react-query';
+import Grid from '@mui/material/Grid';
+import DynamicEventCard from '../../components/Event/DynamicEventCard';
+
+const index = () => {
+  const { place } = useParams();
+
+  const { data, isLoading, isError } = useQuery(['place', place], () => fetchEventByPlace(place!), {
+    staleTime: 3000,
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error loading events</div>;
+  }
+
+  if (!data) {
+    return null;
+  }
+  return (
+    <>
+      <h1>{place}</h1>
+      <div>
+        <Grid sx={{ mt: 4, mx: 'auto' }} container spacing={0}>
+          {data.map((event: any) => (
+            <Grid item xs={12} sm={6} md={3} lg={3} key={event._id} sx={{ padding: 2 }}>
+              <DynamicEventCard event={event} />
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    </>
+  );
+};
+
+export default index;
