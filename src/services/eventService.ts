90a9d78445c21ff7fd65b6d1ec163c25c123029c
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2023-11-18 21:37:08 +0300

    Change baseURL

--- src/services/eventService.ts
+++ src/services/eventService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://plankton-app-ouh3i.ondigitalocean.app/api/',
   headers: {
     'Content-Type': 'application/json',
   },
