f239ec690d7c8e8dce2599ec104fd66652da6635
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2023-12-03 06:55:37 +0300

    add ScrollToTop function

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import Home from './pages/Home';
 import EventDetailsPage from './pages/EventDetailsPage';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import PreviousEventsPage from './pages/PreviousEventsPage';
 import PlacePage from './pages/PlacePage';
 import CategoryPage from './pages/CategoryPage';
 import Particle from './components/Particles/Particle';
+import { useEffect } from 'react';
+
+function ScrollToTop() {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+  return null;
+}
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
