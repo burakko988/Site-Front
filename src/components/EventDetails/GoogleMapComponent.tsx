306f6f689512546f4ed8d5d1b4710bf77334e5d2
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2023-12-03 07:24:27 +0300

    add Google API key to env variables

--- src/components/EventDetails/GoogleMapComponent.tsx
+++ src/components/EventDetails/GoogleMapComponent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
 type Coords = {
@@ -13,12 +13,13 @@ interface GoogleMapComponentProps {
 const containerStyle = {
   height: '350px',
   borderRadius: '20px',
-  border: '8px ridge #01A3A4'
+  border: '8px ridge #01A3A4',
 };
 
 const GoogleMapComponent: React.FC<GoogleMapComponentProps> = ({ coords }) => {
+  const API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
   return (
-    <LoadScript googleMapsApiKey="BURAYA API KEY GİR">
+    <LoadScript googleMapsApiKey={API_KEY}>
       <GoogleMap mapContainerStyle={containerStyle} center={coords} zoom={10}>
         <Marker position={coords} />
       </GoogleMap>
