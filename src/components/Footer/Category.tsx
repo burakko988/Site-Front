c62dc2e61ac60fd16eca88ab84f1fb727eac59e2
MehmetAydar01 <mehmet.aydar07@gmail.com>
2023-12-03 13:59:18 +0300

    classname added

--- src/components/Footer/Category.tsx
+++ src/components/Footer/Category.tsx
@@ -23,79 +23,81 @@ function Category() {
 
   return (
     <Container>
-      <Box sx={{ display: 'flex' }}>
-        <div style={columnStyle}>
-          <h2>Kurumsal</h2>
-          <ul style={listItemStyle}>
-            <li>
-              <a href="#" style={linkStyle} className="hover-link linkStyle">
-                Hakkımızda
-              </a>
-            </li>
-            <li>
-              <a href="#" style={linkStyle} className="hover-link linkStyle">
-                Gizlilik
-              </a>
-            </li>
-            <li>
-              <a href="#" style={linkStyle} className="hover-link linkStyle">
-                Kullanıcı Sözleşmesi
-              </a>
-            </li>
-            <li>
-              <a href="#" style={linkStyle} className="hover-link linkStyle">
-                Kullanım Koşulları
-              </a>
-            </li>
-            <li>
-              <a href="#" style={linkStyle} className="hover-link linkStyle">
-                İletişim
-              </a>
-            </li>
-            <li>
-              <a href="#" style={linkStyle} className="hover-link linkStyle">
-                Çerez Politikası
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div style={columnStyle}>
-          <h2>Kategoriler</h2>
-          <ul style={listItemStyle}>
-            <li>
-              <a href="/previous-events" style={linkStyle} className="hover-link linkStyle">
-                Previous Events
-              </a>
-            </li>
-            <li>
-              <a href="/category/concert" style={linkStyle} className="hover-link linkStyle">
-                Concert
-              </a>
-            </li>
-            <li>
-              <a href="/category/theater" style={linkStyle} className="hover-link linkStyle">
-                Theater
-              </a>
-            </li>
-            <li>
-              <a href="/category/festival" style={linkStyle} className="hover-link linkStyle">
-                Festival
-              </a>
-            </li>
-            <li>
-              <a href="/category/standup" style={linkStyle} className="hover-link linkStyle">
-                Stand Up
-              </a>
-            </li>
-            <li>
-              <a href="/category/sport" style={linkStyle} className="hover-link linkStyle">
-                Sport
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div style={columnStyle}>
-          <p className="hide-paragraph">Web sitemizde en yeni etkinlikleri bulabilir ve bilet satın alabilirsiniz. Her türlü etkinliği kaçırmayın!</p>
+      <Box>
+        <div className="footer-links">
+          <div style={columnStyle}>
+            <h2>Kurumsal</h2>
+            <ul style={listItemStyle}>
+              <li>
+                <a href="#" style={linkStyle} className="hover-link linkStyle">
+                  Hakkımızda
+                </a>
+              </li>
+              <li>
+                <a href="#" style={linkStyle} className="hover-link linkStyle">
+                  Gizlilik
+                </a>
+              </li>
+              <li>
+                <a href="#" style={linkStyle} className="hover-link linkStyle">
+                  Kullanıcı Sözleşmesi
+                </a>
+              </li>
+              <li>
+                <a href="#" style={linkStyle} className="hover-link linkStyle">
+                  Kullanım Koşulları
+                </a>
+              </li>
+              <li>
+                <a href="#" style={linkStyle} className="hover-link linkStyle">
+                  İletişim
+                </a>
+              </li>
+              <li>
+                <a href="#" style={linkStyle} className="hover-link linkStyle">
+                  Çerez Politikası
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div style={columnStyle}>
+            <h2>Kategoriler</h2>
+            <ul style={listItemStyle}>
+              <li>
+                <a href="/previous-events" style={linkStyle} className="hover-link linkStyle">
+                  Previous Events
+                </a>
+              </li>
+              <li>
+                <a href="/category/concert" style={linkStyle} className="hover-link linkStyle">
+                  Concert
+                </a>
+              </li>
+              <li>
+                <a href="/category/theater" style={linkStyle} className="hover-link linkStyle">
+                  Theater
+                </a>
+              </li>
+              <li>
+                <a href="/category/festival" style={linkStyle} className="hover-link linkStyle">
+                  Festival
+                </a>
+              </li>
+              <li>
+                <a href="/category/standup" style={linkStyle} className="hover-link linkStyle">
+                  Stand Up
+                </a>
+              </li>
+              <li>
+                <a href="/category/sport" style={linkStyle} className="hover-link linkStyle">
+                  Sport
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div style={columnStyle}>
+            <p className="hide-paragraph">Web sitemizde en yeni etkinlikleri bulabilir ve bilet satın alabilirsiniz. Her türlü etkinliği kaçırmayın!</p>
+          </div>
         </div>
       </Box>
     </Container>
